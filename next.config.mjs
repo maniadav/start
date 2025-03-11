706806dc76a67dde494c539e239b77487b111780
maniadav <manish.yadav.elit@gmail.com>
2025-03-11 21:09:39 +0530

    deployment fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,16 @@
 // next.config.mjs
 import withSerwist from "@serwist/next";
-// import routes from "../public/routes.json";
+
+const nextConfig = {
+  // next static export logic
+  output: "export",
+  basePath: "/start",
+  // gitHub Pages compatibility
+  trailingSlash: true,
+};
 
 export default withSerwist({
+  // Service Worker configuration
   swSrc: "src/app/sw.ts",
   swDest: "public/sw.js",
-  // precacheManifest: routes.map((url) => ({ url, revision: null })),
-});
+})(nextConfig);
