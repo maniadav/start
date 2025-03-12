ecfc0fda1a98f7c6f4f45599375e9687e61ac50c
maniadav <manish.yadav.elit@gmail.com>
2025-03-12 14:29:40 +0530

    back button

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,84 +1,16 @@
 "use client";
-import { IconHome } from "components/common/Icons";
 import Image from "next/image";
-
-const aboutDetails = [
-  {
-    header: "Medical Research Council",
-    message:
-      "It is a publicly funded government agency responsible for coordinating and funding medical research in the United Kingdom. Council funded the START project by Global Challenge Research Fund Foundation Award",
-  },
-  {
-    header: "University of Reading",
-    message:
-      "The University (located in Berkshire, England) was established in 1892 as a public university. The START project runs under the supervision of the School of Psychology and Clinical Language Science and Professor <b>Bhismadev Chakrabarti</b>.",
-  },
-  {
-    header: "Public Health Foundation of India",
-    message:
-      "PHFI is helping to build institutional and system capacity in India for strengthening education, training, research and policy development in the area of Public Health.<br>Project collaborator: <b>Ph.D Vikram Patel</b>",
-  },
-  {
-    header: "Birkbeck University of London",
-    message:
-      "Birkbeck College is a public research university located in Bloomsbury, London, England, and was established in 1823.<br>Project collaborators: <b>Mark Johnson</b> and <b>Teodora Gliga</b>",
-  },
-  {
-    header: "Nottingham Trent University",
-    message:
-      "The university was formed by the amalgamation of many separate institutions of higher education. It originated from the Nottingham Government School of Design founded in 1843.<br>Project collaborators: <b>Matthew Belmonte</b>",
-  },
-  {
-    header: "All India Institutes of Medical Sciences",
-    message:
-      "The All India Institutes of Medical Sciences (AIIMS) is a group of autonomous public medical colleges of higher education. AIIMS New Delhi, the fore-runner parent excellence institution, was established in 1956.<br>Project collaborators: <b>Sheffali Gulati</b>",
-  },
-  {
-    header: "Sangath",
-    message:
-      "Sangath is a non-governmental, not-for-profit organisation committed to improving health across the life span by empowering existing community resources to provide appropriate physical, psychological and social therapies.<br>Project collaborators: <b>Gauri Divan</b>",
-  },
-  {
-    header: "Indian Institute of Technology Bombay",
-    message:
-      "The IIT Bombay is a public engineering institution located in Powai, Mumbai, India. It is the second-oldest (after Indian Institute of Technology Kharagpur) institute of the Indian Institutes of Technology system.<br>Project collaborators: <b>Sharat Chandran</b>",
-  },
-  {
-    header: "Therapy Box",
-    message:
-      "Therapy Box is a multi award winning software development company, specializing in healthcare and education innovation. We have received numerous international awards for innovation and we bring this expertise in working with universities on complex and special development projects<br>Project participants: <b>Swapnil Gadgil, Rebecca Bright, Nadir Ibrahimov</b>",
-  },
-];
-
-import { useState } from "react";
-import { TasksConstant } from "constants/tasks.constant";
-import TaskHome from "components/TaskHome";
 import SuspenseWrapper from "components/SuspenseWrapper";
 import ProjectParticipants from "./ProjectParticipants";
 import { BASE_URL } from "@constants/config.constant";
+import TopNav from "components/TopNav";
 
 const IndexPage = () => {
-  const data = TasksConstant.ButtonTask;
-  const [survey, setSurvey] = useState<boolean>(false);
-
-  const handleStartGame = () => {
-    setSurvey(!survey);
-  };
-
   return (
     <div className="h-full flex flex-col gap-10">
       <div className="w-full mb-10">
         <div className=" mx-auto h-full sm:p-10">
-          <div className="flex px-4 justify-between items-center">
-            <div className="flex flex-row gap-4 items-center align-middle">
-              <a href="/" className="font-bold text-xl md:text-4xl">
-                <IconHome />
-              </a>
-              <p className="text-xl md:text-4xl font-bold">
-                About<span className="ml-2 text-primary">Us</span>
-              </p>
-            </div>
-          </div>
+          <TopNav primaryText={"About"} secondaryText={"Us"} />
 
           <div className="w-full h-96 pt-10 rounded-xl">
             <Image
