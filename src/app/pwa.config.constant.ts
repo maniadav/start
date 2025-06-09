d6495aa0b86b796458b678d9bb2e363b8f9edff0
maniadav <manish.yadav.elit@gmail.com>
2025-06-10 03:04:19 +0530

    versioning

--- src/app/pwa.config.constant.ts
+++ src/app/pwa.config.constant.ts
@@ -1,3 +1,3 @@
-export const CACHE_NAME = "startbuild";
+export const CACHE_NAME = "start-v1.";
 // Use package version and optional build ID for cache versioning
-export const CACHE_VERSION = "1.0.0";
+export const CACHE_VERSION = "0.0.0";
