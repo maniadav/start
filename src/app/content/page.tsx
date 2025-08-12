c6339d8499abb59b7ab69d581d6c765f05764010
maniadav <manish.yadav.elit@gmail.com>
2025-08-12 17:52:27 +0530

    content page

---
+++ src/app/content/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SurveyDataDetails from "./SurveyDataDetails";
+
+const HomePage = () => {
+  return (
+    <div className="w-full p-8">
+      <div className="container mx-auto py-20 px-4 text-black">
+        <SurveyDataDetails />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
